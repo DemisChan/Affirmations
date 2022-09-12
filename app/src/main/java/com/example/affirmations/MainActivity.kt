2209e8e103538c968370a4a97def5e9363fec783
Demis Lavrentidis <demis.lavrentidis@openbet.com>
2022-09-12 11:11:09 +0100

    Affirmations app completed

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.affirmations.adapter.ItemAdapter
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
+
+
+private const val TAG = "MyActivity ==========>"
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+        Log.i(TAG, "Resource ID ${Affirmation(R.string.affirmation1, R.drawable.image1)}")
+        Log.i(TAG, "Activity $this, $myDataset")
+
+    }
+}
